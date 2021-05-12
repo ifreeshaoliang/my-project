b32f60794d712d989bc62b0395344b0d0b54401a
allen <ifreeshaoliang@163.com>
2021-05-12 20:21:00 +0800

    commit

--- my-project/src/api/sellerAPI.js
+++ my-project/src/api/sellerAPI.js
@@ -4,7 +4,7 @@ import http from '../utils/http'
  *  @parms resquest 请求地址 例如：http://localhost:8081/request/...
  */
 
- let request = "/article/"
+ let request = "/seller/"
 
  export default {
      getUsersInfo(params) {
